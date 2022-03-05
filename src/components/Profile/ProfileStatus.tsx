441c522a76ce77c9b7912ce79dd64eb2b0966bd0
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-03-05 11:11:16 +0600

    componentDidUpdate skip-deep

--- src/components/Profile/ProfileStatus.tsx
+++ src/components/Profile/ProfileStatus.tsx
@@ -5,19 +5,24 @@ type ProfileStatusType = {
     updateStatus: (status: string) => void
 }
 
+type StateType = {
+    editMode: boolean
+    status: string
+}
 
 class ProfileStatus extends React.Component<ProfileStatusType> {
 
-    state ={
+
+    state = {
         editMode: false,
         status: this.props.status
     }
 
     activateEditMode = () => {
-      return this.setState({
-           editMode: true,
+        return this.setState({
+            editMode: true,
 
-       })
+        })
     }
     deActivateEditMode = () => {
 
@@ -29,18 +34,28 @@ class ProfileStatus extends React.Component<ProfileStatusType> {
     }
 
     handleOnchange = (e: ChangeEvent<HTMLInputElement>) => {
-       return this.setState({
-           status: e.currentTarget.value
+        return this.setState({
+            status: e.currentTarget.value
         })
     }
+
+    componentDidUpdate(prevProps: ProfileStatusType, prevState: StateType) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
-        return (  <div>
-            { !this.state.editMode
+        return (<div>
+            {!this.state.editMode
                 ? <div>
                     <span onDoubleClick={this.activateEditMode}>{this.props.status || 'no status'}</span>
                 </div>
-                :<div>
-                <input autoFocus={true} value={this.state.status} onBlur={this.deActivateEditMode} onChange={this.handleOnchange}/>
+                : <div>
+                    <input autoFocus={true} value={this.state.status} onBlur={this.deActivateEditMode}
+                           onChange={this.handleOnchange}/>
                 </div>
             }
         </div>)
