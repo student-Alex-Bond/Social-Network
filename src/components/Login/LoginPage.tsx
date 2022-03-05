242e2e15a5db2b1e6b4f8e8370ae4be837aabd0f
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-03-05 12:19:57 +0600

    connect library redux-form

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import {Field, reduxForm, InjectedFormProps} from "redux-form";
+
+type FormData ={
+    login: string
+    password: string
+    rememberMe: boolean
+}
 
 const LoginPage = () => {
+
+    const onSubmit = (formData: FormData) => {
+        console.log(formData)
+    }
+
     return (
-        <div>
-            Login
+        <div style={{marginLeft: '20px'}}>
+            <h1>Login</h1>
+            <ReduxLoginForm onSubmit={onSubmit}/>
         </div>
     );
 };
 
+const LoginForm = (props: InjectedFormProps<FormData>) => {
+    return (
+        <form onSubmit ={props.handleSubmit} >
+            <div>
+                <Field  name={'login'} type={'text'} placeholder={'login'} component={'input'}/>
+            </div>
+            <div>
+                <Field name={'password'} type={'password'} placeholder={'password'} component={'input'}/>
+            </div>
+            <div>
+                <Field name={'rememberMe'} type={'checkbox'} component={'input'} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const ReduxLoginForm = reduxForm<FormData>({
+    form: 'login'
+})(LoginForm)
 export default LoginPage;
--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -4,12 +4,14 @@ import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunk from "redux-thunk";
+import {reducer as formReducer} from 'redux-form'
 
 let rootReducer =  combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(rootReducer, applyMiddleware(thunk));
