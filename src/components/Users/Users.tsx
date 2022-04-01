a135c6969b27ee6ed17d00da7158f1d448146505
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-04-01 22:39:54 +0600

    delete unused import

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import ava_user from "../../img/ava_person.svg.png";
-import styles from "./Users.module.css";
 import {usersType} from "../../redux/users-reducer";
-import {NavLink} from 'react-router-dom';
 import {Paginator} from "../common/Padinator/Paginator";
 import {User} from "./User";
 
